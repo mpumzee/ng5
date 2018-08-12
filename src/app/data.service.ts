478ec4ba0ff66cb0fe85bc91cf20dd065cf0ce03
Mpumzee <mpumzcode@gmail.com>
2018-08-12 16:20:31 +0200

    another one

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -5,7 +5,7 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root'
 })
 export class DataService {
-  private goals = new BehaviorSubject<any>(['The initial goal', 'Another one']);
+  private goals = new BehaviorSubject<any>(['Flourish in a Zanupf led Country', 'Double my Salary']);
   goal = this.goals.asObservable();
 
   constructor() { }
